9f95ed19af0e318b1bd31205d6b0ad57fc5b0957
mcpe500 <68177813+mcpe500@users.noreply.github.com>
2020-11-05 19:43:55 +0700

    Add files via upload

---
+++ Tugas-4-Functions/Tugas-4-Functions-no-1.js
@@ -0,0 +1,7 @@
+
+function teriak(){
+return "Halo Sanbers!"
+}
+
+ 
+console.log(teriak()) // "Halo Sanbers!"
---
+++ Tugas-4-Functions/Tugas-4-Functions-no-3.js
@@ -0,0 +1,11 @@
+function introduce(name,age,address,hobby){
+return "Nama saya "+name+" umur saya "+age+" tahun, alamat saya di "+address+" , dan saya punya hobby yaitu "+hobby+"!"
+}
+ 
+var name = "Agus"
+var age = 25
+var address = "Jln. Malioboro, Yogyakarta"
+var hobby = "Gaming"
+ 
+var perkenalan = introduce(name, age, address, hobby)
+console.log(perkenalan) // Menampilkan "Nama saya Agus, umur saya 30 tahun, alamat saya di Jln. Malioboro, Yogyakarta, dan saya punya hobby yaitu Gaming!"
